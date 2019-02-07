bdca564ebf0c2f7ca2d2f97167c7bb3a1f8e0ef9
Åsmund Stavdahl <asmund.stavdahl@gmail.com>
2019-02-07 10:50:13 +0100

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # png-to-jpeg
 Convert PNG image to JPEG
+
+# Usage
+```sh
+go get github.com/asmundstavdahl/png-to-jpeg
+go install github.com/asmundstavdahl/png-to-jpeg
+ls
+# iwannabejpeg.png
+png-to-jpeg iwannabejpeg.png yayimjpeg.jpg
+ls
+# iwannabejpeg.png yayimjpeg.jpg
+```
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"image/jpeg"
+	"image/png"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage:\n\t%s png_path jpg_path", os.Args[0])
+		os.Exit(1)
+	}
+
+	inPath := os.Args[1]
+	outPath := os.Args[2]
+
+	reader, err := os.Open(inPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+	img, err := png.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	writer, err := os.OpenFile(outPath, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = jpeg.Encode(writer, img, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
